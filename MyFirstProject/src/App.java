dd0163455485833574d08fe34e0ea05173f18126
Angshu101 <angshu101.9733@gmail.com>
2023-02-09 14:55:16 +0530

    changed made on weighted Graph

--- MyFirstProject/src/App.java
+++ MyFirstProject/src/App.java
@@ -1,12 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-       GraphL graphl =new GraphL(5, true);
+       GraphL graphl =new GraphL(7, true);
         graphl.addEdge(0, 1);
         graphl.addEdge(0, 3);
-        graphl.addEdge(2, 1);
-        graphl.addEdge(3, 2);
-        graphl.addEdge(4, 3);
+        graphl.addEdge(1, 2);
+        graphl.addEdge(2, 3);
+        graphl.addEdge(3, 4);
+        graphl.addEdge(5, 6);
 
-        graphl.PrintMatrix();
+        graphl.bfs();
     }
 }
--- MyFirstProject/src/GraphL.java
+++ MyFirstProject/src/GraphL.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Queue;
+import java.util.*;
 
 //Adjececncy List
 public class GraphL {
@@ -22,6 +24,40 @@ public class GraphL {
     public void PrintMatrix(){
         for(ArrayList<Integer>kuchbi:adj){
             System.out.println(kuchbi.toString());
+        }
+    }
+    public void bfscore(int source, boolean [] visited){
+        //https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/  if the root node is unknown
+        if(visited[source]==false){
+            visited[source]=true;
+        Queue<Integer> pq = new LinkedList<>();
+        pq.add(source);
+        while(pq.size()>0){
+            System.out.println(pq.peek());
+            int top=pq.remove();
+            ArrayList<Integer>neighbours=adj.get(top);
+            for(Integer neighbour:neighbours){
+                if(visited[neighbour]==false){
+                    visited[neighbour]=true;
+                    pq.add(neighbour);
+                }
+            }
+
+        }
+        
+        }
+        
+//time complexity of BFS is O(V+E) v is the number of vertices and E is the number of edges
+// weighted Graph
+//https://www.techiedelight.com/least-cost-path-weighted-digraph-using-bfs/
+    }
+    public void bfs(){
+        boolean visited[]=new boolean[adj.size()];
+        Arrays.fill(visited,false);
+        for(int i=0;i<visited.length;i++){
+            if(visited[i]==false){
+                 bfscore(i, visited);
+            }
         }
     }
 }
